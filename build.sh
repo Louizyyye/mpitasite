fb5fab11afeb854fe51cfb4120a252ad71eecbc1
Dreteck <andredenmark005@gmail.com>
2025-10-03 18:35:33 +0300

    Add build.sh & requirements.txt (prepare Render deploy)

---
+++ build.sh
@@ -0,0 +1,13 @@
+﻿#!/usr/bin/env bash
+# build.sh for Render deployment
+set -e
+
+echo "Installing Python dependencies..."
+pip install --upgrade pip
+pip install -r requirements.txt
+
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
+
+echo "Running migrations..."
+python manage.py migrate --noinput
