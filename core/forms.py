83ed566bd6f0f65f42f71f894f281e5da1fbf26b
Dreteck <andredenmark005@gmail.com>
2025-10-03 19:08:29 +0300

    reformart migrations

--- core/forms.py
+++ core/forms.py
@@ -1,11 +1,25 @@
-# core/forms.py
 from django import forms
 from .models import User
+from django.contrib.auth.forms import AuthenticationForm
 
-class UserRegistrationForm(forms.ModelForm):
+# Patient login form
+class PatientLoginForm(AuthenticationForm):
+    username = forms.CharField(label="Email or Username", widget=forms.TextInput(attrs={
+        "class": "w-full border rounded px-4 py-2",
+        "placeholder": "Email or Username"
+    }))
+    password = forms.CharField(label="Password", widget=forms.PasswordInput(attrs={
+        "class": "w-full border rounded px-4 py-2",
+        "placeholder": "Password"
+    }))
+
+# Patient registration form
+class PatientRegistrationForm(forms.ModelForm):
+    password = forms.CharField(label="Password", widget=forms.PasswordInput(attrs={
+        "class": "w-full border rounded px-4 py-2",
+        "placeholder": "Password"
+    }))
+
     class Meta:
         model = User
-        fields = ['username', 'email', 'password', 'role', 'phone_number', 'occupation']
-        widgets = {
-            'password': forms.PasswordInput(),
-        }
+        fields = ["username", "email", "phone_number", "password"]
